a36df58f4d76b406fb504dd1ed8e59d1ed050fe6
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-08 15:41:24 -0300

    Notificacao SMS

--- src/application/usecases/notificacao_sms.py
+++ src/application/usecases/notificacao_sms.py
@@ -1,10 +1,7 @@
 import logging
 import os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
-import boto3
+from twilio.rest import Client
 
 from infraestructure.repositories.notificacao_interface import NotificacaoInterface
 
@@ -14,30 +11,16 @@ logger = logging.getLogger(__name__)
 
 class NotificacaoSms(NotificacaoInterface):
 
-    def __init__(self, aws_region: str = "us-east-1"):
-        self.ses_client = boto3.client('ses', region_name=aws_region)
+    def enviar_notificacao(self, to: str, nome_usuario: str, message: str):
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        from_ = os.getenv("TWILIO_PHONE_NUMBER")
 
-    def enviar_notificacao(self, to_address: str, nome_usuario: str, message: str):
-        from_address = os.getenv('EMAIL_USER')
-        password = os.getenv('EMAIL_PASSWORD')
+        client = Client(account_sid, auth_token)
 
-        smtp_server = "smtp.gmail.com"
-        smtp_port = 587
-
-        msg = MIMEMultipart()
-        msg['From'] = from_address
-        msg['To'] = to_address
-        msg['Subject'] = "Atualizacao de status de processamento o seu video!"
-        msg.attach(MIMEText(message, 'plain'))
-
-        try:
-            server = smtplib.SMTP(smtp_server, smtp_port)
-            server.starttls()
-            server.login(from_address, password)
-
-            server.sendmail(from_address, to_address, msg.as_string())
-            print("Email enviado com sucesso!")
-        except Exception as e:
-            print(f"Erro ao enviar email: {str(e)}")
-        finally:
-            server.quit()
+        message = client.messages.create(
+            body=message,
+            from_=from_,
+            to=to
+        )
+        print(f"Mensagem enviada com SID: {message.sid}")
--- src/infraestructure/repositories/notificacao_interface.py
+++ src/infraestructure/repositories/notificacao_interface.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class NotificacaoInterface(ABC):
     @abstractmethod
-    def enviar_notificacao(self, to_address: str, nome_usuario: str, message: str):
+    def enviar_notificacao(self, to: str, nome_usuario: str, message: str):
         pass
